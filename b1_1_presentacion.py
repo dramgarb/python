0817cb8518ae7cf6820c9af042bcba7717ffdac6
usuario <daragar05@gmail.com>
2025-09-26 13:57:34 +0200

    ejericio b1_1_presentacion

--- b1_1_presentacion.py
+++ b1_1_presentacion.py
@@ -1,3 +1,3 @@
 nombre = input("¿Como te llamas?")
 edad = input("¿Cuantos años tienes?")
-print(f"Hola {nombre}, tienes {edad} años. Bienvenido a python.")
+print(f"Hola {nombre},, tienes {edad} años. Bienvenido a python.")
