b7ac43062bd48cc57cf534b59a06bc8c15627762
Daniel <danielramirezgarcia005@gmail.com>
2025-09-29 17:19:00 +0200

    nuevos ejercicios terminados

---
+++ b1_2_calculadora_simple.py
@@ -0,0 +1,9 @@
+num1 = int(input("¿Cuál es tu primer número? "))
+num2 = int(input("¿Cuál es tu segundo número? "))
+
+print("Suma:", num1 + num2)
+print("Resta:", num1 - num2)
+print("Multiplicación:", num1 * num2)
+print("División:", num1 / num2)
+print("Módulo:", num1 % num2)
+print("Potencia:", num1 ** num2)
---
+++ ejercicio_guiadoU1-3.py
@@ -0,0 +1,7 @@
+nota = 7
+asistencia = 85
+
+if nota >= 5 and asistencia >= 80:
+    print("Has aprobado")
+else:
+    print("No has aprobado")
