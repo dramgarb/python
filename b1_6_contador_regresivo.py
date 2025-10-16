c8b45849230f614f20c57d6ed5f71c25ce3e18a1
Daniel <danielramirezgarcia005@gmail.com>
2025-10-16 17:00:44 +0200

    nuevos ejercicios terminados falta revisar el de filtrar lista

---
+++ b1_6_contador_regresivo.py
@@ -0,0 +1,4 @@
+num1 = input("Escribe un número: ")
+while int(num1) >= 0:
+    print(num1)
+    num1 = int(num1) - 1
---
+++ b1_7_filtrar_lista.py
@@ -0,0 +1,5 @@
+lista = ["Ana", "Luis", "Carlos", "Alberto", "Marta", "Sofía"]
+for nombre in lista:
+    if nombre == "Ana" or nombre == "Alberto":
+        continue
+    print(nombre)
