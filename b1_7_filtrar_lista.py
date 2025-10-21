3f54fe791230aa4a77e29c7cc2caa80f74ea30d3
Daniel <danielramirezgarcia005@gmail.com>
2025-10-21 16:21:46 +0200

    filtrar lista terminado

--- b1_7_filtrar_lista.py
+++ b1_7_filtrar_lista.py
@@ -1,5 +1,5 @@
 lista = ["Ana", "Luis", "Carlos", "Alberto", "Marta", "Sofía"]
 for nombre in lista:
-    if nombre == "Ana" or nombre == "Alberto":
+    if nombre.startswith('A') or nombre.startswith('a'):
         continue
     print(nombre)
