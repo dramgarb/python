24bf0018f97fa3bcaf727ca03ad2818f34709f52
Daniel <danielramirezgarcia005@gmail.com>
2025-10-21 16:32:31 +0200

    filtrar lista completo

--- b1_7_filtrar_lista.py
+++ b1_7_filtrar_lista.py
@@ -1,4 +1,9 @@
-
+"""
+Este script define una lista de nombres y utiliza un bucle 'for'
+junto con la sentencia 'continue' para mostrar en pantalla solo
+aquellos nombres que no empiezan por la letra 'A' (ni mayúscula
+ni minúscula).
+"""
 lista = ["Ana", "Luis", "Carlos", "Alberto", "Marta", "Sofía"]
 for nombre in lista:
     if nombre.startswith('A') or nombre.startswith('a'):
